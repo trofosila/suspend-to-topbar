7461e1ca0fdd8aaaaae89dfa030038ab81bd3b81
Laurentiu Trofosila <trofosila@gmail.com>
2020-11-02 14:51:18 +0100

    Add zipping process

---
+++ buildzip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -f suspend-to-topbar@zeplia.com.zip
+
+cd suspend-to-topbar@zeplia.com
+
+zip -r suspend-to-topbar@zeplia.com.zip *
+
+cd ..
+
+mv ./suspend-to-topbar@zeplia.com/suspend-to-topbar@zeplia.com.zip .
--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 cp suspend-to-topbar@zeplia.com/ ~/.local/share/gnome-shell/extensions/ -r
